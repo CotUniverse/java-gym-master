ff1fe5d186d4fb94d680b446a3e4ea865d2e62e4
CotU <CotUniverse@yandex.ru>
2026-03-15 12:51:09 +0700

    Изменить HashMap, TreeMap на Map и null на Collections.emptyList()

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,29 +4,29 @@ import java.util.*;
 
 public class Timetable {
 
-    private final HashMap<DayOfWeek, TreeMap<TimeOfDay, List<String>>> timetable = new LinkedHashMap<>();
-    private final HashMap<String, Integer> countTrainingsTable = new HashMap<>();
+    private final Map<DayOfWeek, TreeMap<TimeOfDay, List<String>>> timetable = new LinkedHashMap<>();
+    private final Map<String, Integer> countTrainingsTable = new HashMap<>();
     List<CounterOfTrainings> counterList = new ArrayList<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
         DayOfWeek day = trainingSession.getDayOfWeek();
         TimeOfDay time = trainingSession.getTimeOfDay();
 
-        TreeMap<TimeOfDay, List<String>> daySchedule = timetable.computeIfAbsent(day, k -> new TreeMap<>());
+        Map<TimeOfDay, List<String>> daySchedule = timetable.computeIfAbsent(day, k -> new TreeMap<>());
 
         List<String> training = daySchedule.computeIfAbsent(time, k -> new ArrayList<>());
         training.add(trainingSession.toString());
         //сохраняем занятие в расписании
     }
 
-    public TreeMap<TimeOfDay, List<String>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+    public Map<TimeOfDay, List<String>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
-        return !timetable.isEmpty() ? timetable.get(dayOfWeek) : null;
+        return timetable.getOrDefault(dayOfWeek, new TreeMap<>());
     }
 
     public List<String> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
-        return !timetable.get(dayOfWeek).isEmpty() ? timetable.get(dayOfWeek).get(timeOfDay) : null;
+        return !timetable.get(dayOfWeek).isEmpty() ? timetable.get(dayOfWeek).get(timeOfDay) : Collections.emptyList();
     }
 
     public List<CounterOfTrainings> getCountByCoaches() {
@@ -39,7 +39,7 @@ public class Timetable {
         return counterList;
     }
 
-    public HashMap<String, Integer> countTrainingsForCoaches() {
+    public Map<String, Integer> countTrainingsForCoaches() {
         for (TreeMap<TimeOfDay, List<String>> daySchedule : timetable.values()) {
             for (List<String> sessions : daySchedule.values()) {
                 for (String session : sessions) {
