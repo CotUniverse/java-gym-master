e3b2d1c2b6b05427fba2e10fa9695934d4b96f66
CotU <CotUniverse@yandex.ru>
2026-03-16 11:07:13 +0700

    Изменить List<String> на List<TrainingSession>

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,33 +4,33 @@ import java.util.*;
 
 public class Timetable {
 
-    private final Map<DayOfWeek, TreeMap<TimeOfDay, List<String>>> timetable = new LinkedHashMap<>();
-    private final Map<String, Integer> countTrainingsTable = new HashMap<>();
+    private final Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable = new LinkedHashMap<>();
+    private final Map<Coach, Integer> countTrainingsTable = new HashMap<>();
     List<CounterOfTrainings> counterList = new ArrayList<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
         DayOfWeek day = trainingSession.getDayOfWeek();
         TimeOfDay time = trainingSession.getTimeOfDay();
 
-        Map<TimeOfDay, List<String>> daySchedule = timetable.computeIfAbsent(day, k -> new TreeMap<>());
+        Map<TimeOfDay, List<TrainingSession>> daySchedule = timetable.computeIfAbsent(day, k -> new TreeMap<>());
 
-        List<String> training = daySchedule.computeIfAbsent(time, k -> new ArrayList<>());
-        training.add(trainingSession.toString());
+        List<TrainingSession> training = daySchedule.computeIfAbsent(time, k -> new ArrayList<>());
+        training.add(trainingSession);
         //сохраняем занятие в расписании
     }
 
-    public Map<TimeOfDay, List<String>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+    public Map<TimeOfDay, List<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
         return timetable.getOrDefault(dayOfWeek, new TreeMap<>());
     }
 
-    public List<String> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
         return !timetable.get(dayOfWeek).isEmpty() ? timetable.get(dayOfWeek).get(timeOfDay) : Collections.emptyList();
     }
 
     public List<CounterOfTrainings> getCountByCoaches() {
-        for (Map.Entry<String, Integer> entry : countTrainingsForCoaches().entrySet()) {
+        for (Map.Entry<Coach, Integer> entry : countTrainingsForCoaches().entrySet()) {
             counterList.add(new CounterOfTrainings(entry.getKey(), entry.getValue()));
         }
 
@@ -39,15 +39,14 @@ public class Timetable {
         return counterList;
     }
 
-    public Map<String, Integer> countTrainingsForCoaches() {
-        for (TreeMap<TimeOfDay, List<String>> daySchedule : timetable.values()) {
-            for (List<String> sessions : daySchedule.values()) {
-                for (String session : sessions) {
-                    String[] parts = session.split("Ф.И.О");
-                    for (int i = 1; i < parts.length; i++) {
-                        String coach = parts[i].trim();
-                        countTrainingsTable.put(coach, countTrainingsTable.getOrDefault(coach, 0) + 1);
-                    }
+    public Map<Coach, Integer> countTrainingsForCoaches() {
+        countTrainingsTable.clear();
+
+        for (TreeMap<TimeOfDay, List<TrainingSession>> daySchedule : timetable.values()) {
+            for (List<TrainingSession> sessions : daySchedule.values()) {
+                for (TrainingSession session : sessions) {
+                    Coach coach = session.getCoach();
+                    countTrainingsTable.put(coach, countTrainingsTable.getOrDefault(coach, 0) + 1);
                 }
             }
         }
